3ce7ba7f65bb0ab842b7934ea36c8b4eb8ee52ef
natilou <30585029+natilou@users.noreply.github.com>
2022-04-27 18:48:15 -0300

    Creation of login and logout view with Token.

--- myproject/brightnews/urls.py
+++ myproject/brightnews/urls.py
@@ -1,8 +1,13 @@
-from .views import RegisterAPI
+from .views import RegisterAPI, LoginAPI
 from django.urls import path
+from knox import views
+
 
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', views.LogoutAllView.as_view(), name='logoutall'),
 ]
 
 # urlpatterns = [
